aa51e48122531650a9231e4c5ab3403eafafaacb
jonytalukdar <jonytalukdar30@gmail.com>
2021-09-09 17:10:23 +0600

    completed project structrure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 const buildStyles = () => {
-  return src('*.scss').pipe(sass()).pipe(dest('css'));
+  return src('jerry/**/*.scss').pipe(sass()).pipe(dest('css'));
 };
 
 const watchTask = () => {
-  watch(['*.scss'], buildStyles);
+  watch(['jerry/**/*.scss'], buildStyles);
 };
 
 exports.default = series(buildStyles, watchTask);
